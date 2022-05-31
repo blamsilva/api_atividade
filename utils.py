c11d3c995c31709ae4c80ec2eca90fee78f63ae4
blamsilva <blamsilva@terra.com.br>
2022-05-31 03:44:08 -0300

    Alterado nossa REST API para realizar autenticação no banco de Dados. Em models foi criado tabela de usuários com login e senha para autenticação nas APIS.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 # Insere dados na tabela pessoa
 def insere_pessoas():
@@ -24,8 +24,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Bruno').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
+    #insere_usuario('Bruno', '1234')
+    #insere_usuario('Alice', '4321')
+    consulta_todos_usuarios()
     #insere_pessoas()
-    exclui_pessoa()
-    consulta_pessoas()
+    #exclui_pessoa()
+    #consulta_pessoas()
     #altera_pessoa()
